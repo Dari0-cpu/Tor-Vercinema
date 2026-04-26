2cec579bd3b04d76353644f291e1131db7658abb
Pimpi <pimpinidario@gmail.com>
2026-04-26 17:21:33 +0200

    Sito web di prova

---
+++ WEB/script.js
@@ -0,0 +1,10 @@
+// Aggiunge un effetto di background alla navbar quando si scorre
+const navbar = document.getElementById('navbar');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
